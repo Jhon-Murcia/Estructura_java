f35fbf95122f24f58f1d9635a03607cb87fadd44
Jhon-Murcia <mjhon@ucundinamarca.edu.co>
2025-02-27 18:42:43 -0500

    Ejercicio 2

--- src/main/java/org/example/Ejercicio_1.java
+++ src/main/java/org/example/Ejercicio_1.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Ejercicio_1 {
     public static void main(String[] args) {
-        System.out.println("Este es el ejercicio 1");
+        System.out.println("Este es el ejercicio 2 de tipos de datos");
     }
 }
