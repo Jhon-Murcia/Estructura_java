cc93af0a9426f291a58a5e3c590d0d338034f773
Jhon-Murcia <mjhon@ucundinamarca.edu.co>
2025-02-28 12:56:58 -0500

    primera parte

--- src/main/java/org/example/Ejercicio_calculadora.java
+++ src/main/java/org/example/Ejercicio_calculadora.java
@@ -1,8 +1,39 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Ejercicio_calculadora {
     public static void main(String[] args) {
 
-        System.out.println("Este realiza 8 operaciones");
+        System.out.println("Este ejercicio ejecuta una calculadora que realiza 8 operaciones");
+
+        Scanner entrada=new Scanner(System.in);
+        boolean continuar=true;
+
+        while (continuar) {
+            System.out.println("Menu");
+            System.out.println("1.Suma");
+            System.out.println("2.Resta");
+            System.out.println("3.Multiplicacion");
+            System.out.println("4.Division");
+            System.out.println("5.Potenciacion");
+            System.out.println("6.Raiz cuadrada");
+            System.out.println("7.Modulo");
+            System.out.println("8.Conversion de grados Celsius a Fahrenheit");
+            System.out.println("9.Salir");
+
+            int opcion= entrada.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Ingresa el primer numero");
+                    double num1= entrada.nextDouble();
+                    System.out.println("Ingresa el segundo numero");
+                    double num2= entrada.nextDouble();
+                    System.out.println("Resultado: "+(num1+num2));
+                    break;
+
+            }
+        }
     }
 }
