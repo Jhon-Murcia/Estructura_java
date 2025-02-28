ab13ca597fda43b826d4f68e3d2e27111b8e08b6
Jhon-Murcia <mjhon@ucundinamarca.edu.co>
2025-02-28 09:34:41 -0500

    cambio_de_nombre

--- src/main/java/org/example/Ejercicio_2.java
+++ src/main/java/org/example/Ejercicio_2.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class Ejercicio_1 {
+public class Ejercicio_2 {
     public static void main(String[] args) {
         System.out.println("Este es el ejercicio 2 de tipos de datos");
     }
