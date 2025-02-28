75d0005bc8f68d19b1b7f5089d55e8a47b169c5f
Oscar <juliangomes8090@gmail.com>
2025-02-28 09:50:26 -0500

    Ejercicio_3

--- src/main/java/org/example/Ejercicio_3.java
+++ src/main/java/org/example/Ejercicio_3.java
@@ -1,7 +1,38 @@
 package org.example;
-
+import java.util.Scanner;
 public class Ejercicio_3 {
+
     public static void main(String[] args) {
-        System.out.println("Este es el ejercicio 2 de tipos de datos");
+            System.out.println("Hello World!");
+            Scanner scanner = new Scanner(System.in);
+
+            int numero;
+            boolean esPrimo = true;
+
+            System.out.println("Ingrese un numero entero");
+            numero = scanner.nextInt();
+
+            if(numero<=1){
+                esPrimo = false;
+
+            } else {
+                for(int i = 2; i <= numero/2;i++){
+                    if(numero % i ==0){
+                        esPrimo = false;
+                        break;
+                    }
+                }
+
+            }
+
+            //estructura de control con if-else
+            if(esPrimo){
+                System.out.println(numero+ "es numero primo");
+
+            } else {
+                System.out.println(numero + "no es numero primo");
+            }
+
+            scanner.close();
+        }
     }
-}
