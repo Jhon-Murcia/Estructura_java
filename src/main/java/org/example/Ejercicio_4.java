eecdf3480c499e5d8ab8b5b76d2c1401c291105b
Jhon-Murcia <mjhon@ucundinamarca.edu.co>
2025-02-28 09:24:30 -0500

    .

---
+++ src/main/java/org/example/Ejercicio_4.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Ejercicio_4 {
+    public static void main(String[] args) {
+        System.out.println("Este es el ejercicio 4 de tipos de datos");
+    }
+}
---
+++ src/main/java/org/example/Ejercicio_calculadora.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Ejercicio_calculadora {
+    public static void main(String[] args) {
+        System.out.println("Este realiza 8 operaciones");
+    }
+}
